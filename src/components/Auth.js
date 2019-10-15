bdb1dcb206377a33157f08f64eaf892b0e557edc
anis <smail.a.anis@gmail.com>
2019-10-16 00:03:49 +0200

    refactor Auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,67 +6,17 @@ import "firebase/auth";
 import store from "../store.js";
 import connect from "react-redux/es/connect/connect";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import signInWithGoogle from "../signinWithGoogle";
 
-let token;
-let user;
-
-function setUser(user) {
-  console.log("setUser", user);
-  store.dispatch({ type: "FETCH_USER_SUCCESS", ...user });
-}
-
-function signInWithGoogle() {
-  store.dispatch({ type: "FETCH_USER" });
-  firebase
-    .auth()
-    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
-    .then(function() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      // In memory persistence will be applied to the signed in Google user
-      // even though the persistence was set to 'none' and a page redirect
-      // occurred.
-      return firebase.auth().signInWithPopup(provider);
-    })
-    .then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      token = result.credential.accessToken;
-      // The signed-in user info.
-      user = result.user;
-      console.log("auth success", { token, user });
-      if (user) {
-        const uid = user.uid;
-        const email = user.email;
-        const display_name = user.displayName;
-        console.log("User is signed in." + uid);
-        setUser({
-          uid,
-          display_name,
-          email
-        });
-      } else {
-        store.dispatch({ type: "FETCH_USER_ERROR" });
-        console.log("User is signed out.");
-      }
-
-      // ...
-    })
-    .catch(function(error) {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-      console.error({
-        errorCode,
-        errorMessage,
-        email,
-        credential
-      });
-      store.dispatch({ type: "FETCH_USER_ERROR" });
-      // ...
+function handleOnClickSignIn() {
+  signInWithGoogle().then(user => {
+    store.dispatch({
+      type: "FETCH_USER_SUCCESS",
+      uid: user.uid,
+      display_name: user.displayName,
+      email: user.email
     });
+  });
 }
 
 const Auth = ({ fetchingConnectionState }) => {
@@ -96,7 +46,7 @@ const Auth = ({ fetchingConnectionState }) => {
       spacing={8}
     >
       <Grid item xs={12}>
-        <GoogleLoginButton onClick={signInWithGoogle} />
+        <GoogleLoginButton onClick={handleOnClickSignIn} />
       </Grid>
     </Grid>
   );
---
+++ src/signinWithGoogle.js
@@ -0,0 +1,40 @@
+import store from "./store";
+import * as firebase from "firebase";
+
+export default function signInWithGoogle() {
+  store.dispatch({ type: "FETCH_USER" });
+  return firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      // In memory persistence will be applied to the signed in Google user
+      // even though the persistence was set to 'none' and a page redirect
+      // occurred.
+      return firebase.auth().signInWithPopup(provider);
+    })
+    .then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      let token = result.credential.accessToken;
+      // The signed-in user info.
+      let user = result.user;
+      console.log("auth success", { token, user });
+      return user;
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      console.error({
+        errorCode,
+        errorMessage,
+        email,
+        credential
+      });
+      store.dispatch({ type: "FETCH_USER_ERROR" });
+    });
+}
