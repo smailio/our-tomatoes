bc9fd504e3f8fd414754b140fbf9fa7f1323449f
anis <smail.a.anis@gmail.com>
2020-01-20 23:59:10 +0100

    better layout

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -4,21 +4,33 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import MyPage from "./MyPage";
 import OtherGuyPage from "./OtherGuyPage";
 import { useObserveFollowing } from "../useObserveFollowing";
+import { Grid } from "@material-ui/core";
 
 const Hello = ({ uid }) => {
   useObserveFollowing();
   return (
-    <Switch>
-      <Route path={`/${uid}`}>
-        <MyPage />
-      </Route>
-      <Route path="/:uid">
-        <OtherGuyPage />
-      </Route>
-      <Route path="/">
-        <Redirect to={{ pathname: `/${uid}` }} />
-      </Route>
-    </Switch>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ marginTop: "20vh" }}
+      spacing={8}
+    >
+      <Grid item xs={12}>
+        <Switch>
+          <Route path={`/${uid}`}>
+            <MyPage />
+          </Route>
+          <Route path="/:uid">
+            <OtherGuyPage />
+          </Route>
+          <Route path="/">
+            <Redirect to={{ pathname: `/${uid}` }} />
+          </Route>
+        </Switch>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import MyTomato from "./MyTomato";
 import ControlPanel from "./ControlPanel";
 import Following from "./Following";
+import { Grid } from "@material-ui/core";
 
 const MyPage = () => {
   const [off_label, set_off_label] = useState("OFF");
   return (
-    <div>
-      <MyTomato
-        off_label={off_label}
-        over_write_label={off_label !== "OFF" ? off_label : ""}
-      />
-      <ControlPanel set_off_label={set_off_label} />
-      <Following />
-    </div>
+    <Grid container alignItems="center" justify="center" spacing={8}>
+      <Grid item xs={4}>
+        <MyTomato
+          off_label={off_label}
+          over_write_label={off_label !== "OFF" ? off_label : ""}
+        />
+      </Grid>
+      <Grid item xs={12} />
+      <Grid item xs={8}>
+        <ControlPanel set_off_label={set_off_label} />
+      </Grid>
+      <Grid item xs={8}>
+        <Following />
+      </Grid>
+    </Grid>
   );
 };
 
