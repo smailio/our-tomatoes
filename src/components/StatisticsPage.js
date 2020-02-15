0ec779b16a3edda891035c7b488e2f3ea32876cd
anis <smail.a.anis@gmail.com>
2020-02-15 09:51:30 +0100

    replace dummy from date and to date by first tomato date and now

--- src/components/StatisticsPage.js
+++ src/components/StatisticsPage.js
@@ -10,7 +10,7 @@ function get_day_date(d) {
 
 function count_successfyll_tomatoes_per_day(tomatoes) {
   console.log("count per day", tomatoes);
-  const l = _.chain(tomatoes.value)
+  const l = _.chain(tomatoes)
     .filter(
       tomato => tomato.start_time !== undefined && tomato.end_time !== undefined
     )
@@ -28,9 +28,17 @@ function count_successfyll_tomatoes_per_day(tomatoes) {
   return l;
 }
 
+function get_fitst_tomato_date(my_tomatoes) {
+  return get_day_date(
+    _.min(my_tomatoes.map(tomato => tomato.start_time.toDate()))
+  );
+}
+
+// function get_last_tomato_date(my_tomatoes) {}
+
 function StatisticsPage() {
   const my_tomatoes = useMyTomatoes();
-  if (my_tomatoes.is_loading) {
+  if (my_tomatoes.value === undefined || my_tomatoes.is_loading) {
     return (
       <div>
         <CircularProgress />
@@ -38,15 +46,18 @@ function StatisticsPage() {
     );
   }
 
+  const from = get_fitst_tomato_date(my_tomatoes.value);
+  const to = get_day_date(new Date());
+
   return (
     <div style={{ height: "33vh", width: "95vw" }}>
       <ResponsiveCalendar
-        data={count_successfyll_tomatoes_per_day(my_tomatoes)}
-        from="2019-03-01"
-        to="2020-07-12"
+        data={count_successfyll_tomatoes_per_day(my_tomatoes.value)}
+        from={from}
+        to={to}
         emptyColor="#eeeeee"
         colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
-        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        margin={{ top: 40, right: 10, bottom: 10, left: 10 }}
         yearSpacing={40}
         monthBorderColor="#ffffff"
         dayBorderWidth={2}
